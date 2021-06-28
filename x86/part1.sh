aa7b91447ac0b4dd2c31809a6cd04212d0bde047
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-06-28 10:34:22 +0800

    添加vlmcsd 及其 luci 界面库。

--- x86/part1.sh
+++ x86/part1.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# vlmcsd 及其 luci 界面
+git clone --depth=1 https://github.com/mchome/openwrt-vlmcsd package/vlmcsd
+git clone --depth=1 https://github.com/mchome/luci-app-vlmcsd package/luci-app-vlmcsd
+
 # ddns-scripts_dnspod OpenWrt 官方 DDNS 插件的 dnspod_cn 更新脚本
 git clone -b v0.0.1 --depth=1 https://github.com/VergilGao/ddns-scripts_dnspod package/ddns-scripts_dnspod
