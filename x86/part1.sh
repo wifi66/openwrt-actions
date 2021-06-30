fad457454d168d0e48a215b84f45c94cd7d06194
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-06-30 09:08:29 +0800

    update dnsfilter to 1.0-10

--- x86/part1.sh
+++ x86/part1.sh
@@ -8,7 +8,7 @@ git clone --depth=1 https://github.com/mchome/luci-app-vlmcsd package/luci-app-v
 git clone -b v0.0.1 --depth=1 https://github.com/VergilGao/ddns-scripts_dnspod package/ddns-scripts_dnspod
 
 # dnsfilter 基于DNS的广告过滤
-git clone -b 1.0-8 --depth=1 https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
+git clone -b 1.0-10 --depth=1 https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
 
 # luci-theme-edge 一个清爽的luci主题
 git clone -b v2.5-19.07 --depth=1 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
