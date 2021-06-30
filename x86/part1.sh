6285c7c3ecd3a1a48f98b42440483be756df9c71
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-06-30 09:10:29 +0800

    改用small-5修改过的ddns-scripts-dnspod库。

--- x86/part1.sh
+++ x86/part1.sh
@@ -4,8 +4,8 @@
 git clone --depth=1 https://github.com/mchome/openwrt-vlmcsd package/vlmcsd
 git clone --depth=1 https://github.com/mchome/luci-app-vlmcsd package/luci-app-vlmcsd
 
-# ddns-scripts_dnspod OpenWrt 官方 DDNS 插件的 dnspod_cn 更新脚本
-git clone -b v0.0.1 --depth=1 https://github.com/VergilGao/ddns-scripts_dnspod package/ddns-scripts_dnspod
+# ddns-scripts-dnspod OpenWrt 官方 DDNS 插件的 dnspod_cn 更新脚本
+git clone --depth=1 https://github.com/small-5/ddns-scripts-dnspod package/ddns-scripts-dnspod
 
 # dnsfilter 基于DNS的广告过滤
 git clone -b 1.0-10 --depth=1 https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
