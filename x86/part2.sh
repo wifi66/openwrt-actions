c5bd6e797ffddba03181585457d8c5d01c1ed96d
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-07-21 09:14:24 +0800

    修正openclash依赖

--- x86/part2.sh
+++ x86/part2.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# get libcap from master branch
+rm -rf feeds/packages/libs/libcap
+svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
+
+# Modify DHCP client 
+sed -i 's/\${vendorid:+-V "$vendorid"}/-V \"\"/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
